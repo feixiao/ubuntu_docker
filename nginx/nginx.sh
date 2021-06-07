909dba65c7a093f0ae87d3c31741ecde5eb1ccca
feixiao <feixiao2020@sina.com>
2021-06-07 13:29:54 +0800

    添加运行脚本

---
+++ nginx/nginx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+export ENV=localhost
+docker run --rm -p 8080:80  -p 443:443 \
+    -v $PWD/nginx/${ENV}/nginx.conf:/etc/nginx/nginx.conf \
+    -v $PWD/www/${ENV}:/usr/share/nginx/html    \
+    -v $PWD/ssl/${ENV}:/ect/nginx/cert   \
+    --name nginx nginx:mainline-alpine
