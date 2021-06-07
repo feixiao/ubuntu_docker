220f3cbabbfaa74d6b3b0cd854f9892ee8cf4653
feixiao <feixiao2020@sina.com>
2021-06-07 13:50:41 +0800

    修改默认配置

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 export ENV=localhost
-docker run --rm -p 8080:80  -p 443:443 \
+docker run --rm -p 8080:80  itd -p 443:443 \
     -v $PWD/nginx/${ENV}/nginx.conf:/etc/nginx/nginx.conf \
     -v $PWD/www/${ENV}:/usr/share/nginx/html    \
     -v $PWD/ssl/${ENV}:/ect/nginx/cert   \
